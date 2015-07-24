32b013e3aa4acbf5003724290be9316a1bec2a44
Dee Bryant <tolkadot@gmail.com>
2015-07-24 12:35:25 +1000

    Update chunky.js

--- chunky.js
+++ chunky.js
@@ -1,34 +1,22 @@
 function chunk(arr, size) {
-    
+    var i = 0;
     var arr1 = [];
     var arr2 = [];
-  var i = 0;
-  //
-  //{
-  //console.log(j);
-  
-     
        for (j=0; j<(arr.length)/size; j++)
         {
-            
-           arr2.push(arr[i], arr[i+1]);
-           console.log(arr2);
-           arr1[j] = arr2;
-           arr2 = [];
-           console.log(arr1);
-            
-            
-             i = i+size;
-            //moo.push([j], foo[i]); 
-        }
- 
-   //console.log(arr1);
-   // console.log(arr2);
- // }
-   
-  
-  
-  return arr1;
+          
+            for(z=0; z<size; z++)
+            {
+                if(i <= arr.length -1)
+                    {
+                     arr2.push(arr[i]);
+                     i++;
+                    }
+            }
+        arr1[j] = arr2;
+        arr2 = [];
+ }
+ return arr1;
 }
 
-chunk(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], 2);
+chunk([0, 1, 2, 3, 4, 5], 4);
