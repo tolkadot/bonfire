f321120179166e74c86cdf5c64cf74c57d6fc644
Dee Bryant <tolkadot@gmail.com>
2015-08-02 18:41:35 +1000

    Update whereArtThou.js

--- whereArtThou.js
+++ whereArtThou.js
@@ -1,27 +1,19 @@
 function where(collection, source) {
   var arr = [];
-  var arr2 = Object.keys(collection);
-  // arr2 = ["0", "1", "2", "3"]
+  var arr1 = Object.keys(collection);
+  var arr2 = [];
+  // arr1 = ["0", "1", "2", "3"]
 
 
-  for (var foo in source) {
-  if (source.hasOwnProperty(foo)) {
-   var bar = foo;
 
 for(var key in source) {
-    var val = source[key];
-    console.log(val);
-
+   arr2.push(source[key]);
+   // console.log(arr2); // = ["Capulet"]
+   console.log(key);
 }
    
    
-   
-   //console.log(source[foo]);
-   //Capulet
-   //console.log(foo); //console.log(bar);
-   //last
-  }
-  }
+  
  
   for(var i=0; i<arr2.length; i++)
   {
@@ -51,5 +43,5 @@ where([{ first: 'Romeo',
           last: 'Capulet' }], 
               { last: 'Capulet' });
               
-where([{ 'a': 1 }, { 'a': 1 }, { 'a': 1, 'b': 2 }], 
-{ 'a': 1 });
+//where([{ 'a': 1 }, { 'a': 1 }, { 'a': 1, 'b': 2 }], 
+//{ 'a': 1 });
