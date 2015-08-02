62f6d5bbfb2dad4a973eb8cea7a2c52b1d866043
Dee Bryant <tolkadot@gmail.com>
2015-08-02 19:27:29 +1000

    Update whereArtThou.js

--- whereArtThou.js
+++ whereArtThou.js
@@ -1,29 +1,28 @@
 function where(collection, source) {
   var arr = [];
   var arr1 = Object.keys(collection);
-  var arr2 = [];
-  // arr1 = ["0", "1", "2", "3"]
-
-
-
-for(var key in source) {
-   arr2.push(source[key]);
-   // console.log(arr2); // = ["Capulet"]
-   console.log(key);
-}
-   
-   
   
- 
-  for(var i=0; i<arr2.length; i++)
+for(var key in source) {
+  
+  for(var i=0; i<arr1.length; i++)
   {
-     //console.log(source.bar);
-      //if(collection[arr2[i]].bar === source.bar)
-      //{
-      //arr.push(collection[arr2[i]].bar)
-      //console.log(arr)
-      //}
+     if(collection[arr1[i]][key] === source[key])
+      {
+      arr.push(collection[arr1[i]])
+      console.log(collection[arr1[i]])
+      }
   }
+   return arr;    
+  }
+}
+  
+where([ { first: 'Romeo', last: 'Montague' }, 
+        { first: 'Mercutio', last: null },
+        { first: 'Tybalt', last: 'Capulet' },], 
+              { last: 'Capulet' });
+              
+where([{ 'a': 1 }, { 'a': 1 }, { 'a': 1, 'b': 2 }], 
+{ 'a': 1 });
   
   
       
