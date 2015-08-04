ffb72d8f944b0c5a5d9d577ce6d0c85f8539bd07
Dee Bryant <tolkadot@gmail.com>
2015-08-04 20:26:11 +1000

    Update whereDoIBelong.js

--- whereDoIBelong.js
+++ whereDoIBelong.js
@@ -1,22 +1,18 @@
 function where(arr, num) {
- 
- arr.sort(function(a, b){
-     console.log(arr);
-     return a-b;
-     });
+ var place;
+arr.sort(function(a, b) {
+  return a - b;
+});
+
+
 for(i=0; i<arr.length; i++){
-    if(num < arr[i]) 
+    if(num > arr[i]) 
      {
-         console.log(i);
+         place = i +1;
      }
 }
- 
- 
- 
- 
-   return num;
+  num = place;
+  return num;
 }
 
 where([40, 60, 80, 10], 50);
-
-
