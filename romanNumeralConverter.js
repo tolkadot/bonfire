656db4005f81a79f5050a8a65c21d82b7e01d6b9
Dee Bryant <tolkadot@gmail.com>
2015-08-05 21:08:32 +1000

    Update romanNumeralConverter.js

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,31 +1,24 @@
 function convert(num) {
- //return num;
  arr2 =[];
- var arr = ['0', 'I', "II","III", 'IV', "V", "VI", "VII", "VIII", "IX", "X"];
+ var arr = ['0', 'I', "II","III", 'IV', "V", "VI", "VII", "VIII", "IX", "X", "L", "C"];
  num = num.toString().split('');
- 
- //console.log(num);
- 
 //units
  arr2.push(arr[num[(num.length - 1)]]);
- 
-//tens
+//tens up to 30
  var tens = (num[num.length - 2]);
  for (i=0; i<tens; i++)
   arr2.push(arr[10]);
+ num = arr2.reverse().toString().replace(/,/gi, '');
  
-//console.log(tens);
-console.log(arr2);
- 
- 
- 
+ return(num);
 }
 
-convert(36);
 convert(12);
-convert(5);
-convert(9)
+//convert(5);
+//convert(9)
 
-convert(29);
-convert(16);
+//convert(29);
+//convert(16);
+//convert(36);
+
 
