5032f2bc2f680f47f52bfe5011f65e060a2d097e
Dee Bryant <tolkadot@gmail.com>
2015-08-05 19:03:43 +1000

    Update diffTwoArrays.js

--- diffTwoArrays.js
+++ diffTwoArrays.js
@@ -1,30 +1,31 @@
 function diff(arr1, arr2) {
   var newArr = [];
 
-//console.log(arr1.length); 
-//console.log(arr2.length);
-
 for(i=0; i<arr2.length; i++) {
     
-        if (arr1.indexOf(arr2[i]) != -1) {
-            console.log(arr1.indexOf(arr2[i]));
-            
+        if (arr1.indexOf(arr2[i]) === -1) {
+            //console.log(i);
+            console.log(arr2[i]);
+            newArr.push(arr2[i]);
+            console.log(newArr);
+        }
+}
+        for(i=0; i<arr1.length; i++) {
+    
+        if (arr2.indexOf(arr1[i]) === -1) {
+            //console.log(i);
+            console.log(arr1[i]);
+            newArr.push(arr1[i]);
+            console.log(newArr);
         }
         
     
-    
+    console.log(newArr);
 }
 
-if(arr1[0] === arr2[0]){
-    //console.log(arr2[0]);
-}
+
+
 
  
-  //return newArr;
+  return newArr;
 }
-
-diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
-diff(['andesite', 'grass', 'dirt', 'dead shrub'],
- ['andesite', 'grass', 'dirt', 'dead shrub']);
- diff([1, 'calf', 3, 'piglet'], 
- [1, 'calf', 3, 4]);
