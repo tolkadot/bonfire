c37c66839bb4c7f235f7bc91ce6975e7d0927c8d
Dee Bryant <tolkadot@gmail.com>
2015-08-05 14:00:41 +1000

    Update sumOfAllNumbers.js

--- sumOfAllNumbers.js
+++ sumOfAllNumbers.js
@@ -1,30 +1,12 @@
 function sumAll(arr) {
+arr = arr.sort(function(a, b){return a-b});
 
-/*  var total = arr[0];
-  var counter = 0;
-  while(counter<(arr[1] - arr[0] +1)){
-      total = total+1;
-      counter = counter+1;
-  }*/
-        
-        
-        var y = arr[1];
-        var total = arr[0];
-        
-        
-        function sum(x){
-            for(var i = x; i<y; i++) {
-           total = x + (sum(x+1)) ;
-           console.log(total);
-        }
-        sum(2);
-        }
-        
-     return total;  
+var total = 0;
+for( i=arr[0]; i<arr[1]+1; i++)
+{
+    total+=i;
+}
+    return total;  
    }
-  
-  
-  
-
-
+sumAll([10, 5]);
 sumAll([1, 7]);
