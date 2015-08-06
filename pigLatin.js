7eac7e5e2bce9f0fa6ef3464ae47c48101901bca
Dee Bryant <tolkadot@gmail.com>
2015-08-06 20:09:40 +1000

    Create pigLatin.js

---
+++ pigLatin.js
@@ -0,0 +1,37 @@
+
+
+function translate(str) {
+   var arr = str.split('');
+    console.log(arr[0]);
+    var arrOfVowels = ['a', 'e', 'i', 'o', 'u'];
+    console.log(arrOfVowels[0]);
+    console.log(arrOfVowels.length);
+    
+    
+  for(i=0; i<arrOfVowels.length; i++){
+    // console.log(arrOfVowels[i]);
+    }
+     /*if(arr[0] === arrOfVowels[i]){
+         console.log(arrOfVowels[i]);
+         arr = arr.push('w', 'a', 'y');
+     }*/
+   
+    //console.log(arr);
+    //console.log(arr.indexOf(a||e||i||o||u));
+    
+    
+    
+ //return str;
+}
+
+//translate("consonant");
+translate("algorithm");
+
+
+/*translate("california")// 'aliforniacay'
+/translate("paragraphs")// 'aragraphspay'
+translate("glove")// 'oveglay'
+translate("algorithm")// 'algorithmway'
+translate("eight")// 'eightway'
+translate("trash") // "ashtray"
+*/
