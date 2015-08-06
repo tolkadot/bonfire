ac40b50312d95d97b9489abbaac855b58ef2e97b
Dee Bryant <tolkadot@gmail.com>
2015-08-06 13:43:49 +1000

    Update romanNumeralConverter.js

--- romanNumeralConverter.js
+++ romanNumeralConverter.js
@@ -1,24 +1,48 @@
+//convert to roman numerals up to & including 100
+
 function convert(num) {
  arr2 =[];
- var arr = ['0', 'I', "II","III", 'IV', "V", "VI", "VII", "VIII", "IX", "X", "L", "C"];
+ var arr = ['', 'I', "II","III", 'IV', "V", "VI", "VII", "VIII", "IX", "X", "L", "C"];
  num = num.toString().split('');
-//units
- arr2.push(arr[num[(num.length - 1)]]);
-//tens up to 30
- var tens = (num[num.length - 2]);
- for (i=0; i<tens; i++)
-  arr2.push(arr[10]);
- num = arr2.reverse().toString().replace(/,/gi, '');
  
+//units
+ arr2.push(arr[num[(num.length - 1)]]); 
+ var tens = (num[num.length - 2]);
+ 
+ //case of 100
+ if(tens < 1){
+    arr2.push(arr[12]);}
+     
+  //tens up to 30  
+    else if(tens<=3){
+    for (i=0; i<tens; i++){
+     arr2.push(arr[10]);}}
+ 
+ //tens up to 40
+  else if(tens <=4){
+  arr2.push(arr[11]);
+  arr2.push(arr[10]);}
+        //tens up to 50
+        else if(tens <=5){
+        arr2.push(arr[11]);}
+            //tens up to 80
+            else if(tens <9){
+            for (i=5; i<tens; i++){
+            arr2.push(arr[10]);}
+            arr2.push(arr[11]);}
+                 //tens up to 90
+                else if(tens < 10){
+                arr2.push(arr[12]);
+                arr2.push(arr[10]);}
+                
+ num = arr2.reverse().toString().replace(/,/gi, '');
  return(num);
 }
 
-convert(12);
+convert(75);
 //convert(5);
 //convert(9)
 
 //convert(29);
 //convert(16);
 //convert(36);
-
-
