6ea09a704ea6da0ddce084a14e9310d2088e2f8f
Dee Bryant <tolkadot@gmail.com>
2015-08-07 13:31:19 +1000

    Update missingLetter.js

--- missingLetter.js
+++ missingLetter.js
@@ -3,36 +3,40 @@
 
 function fearNotLetter(str) 
 {
-  var arrOfInput = [];
+  var arrOfInput = []; 
   //console.log(str);
   for (var i=0; i<str.length; i++)
   {
     //console.log(str.charCodeAt(i));
-    arrOfInput.push(str.charCodeAt(i));
+    arrOfInput.push(str.charCodeAt(i)); //push unicode vals to array
   //console.log(str.charCodeAt(i));
-  console.log(arrOfInput);
+  //console.log(arrOfInput);
   }
    
-   for(i=0; i<arrOfInput.length -1; i++)
+   for(i=0; i<arrOfInput.length -1; i++) //for each member of the array
    { 
-      console.log(arrOfInput[i+1]);
-      console.log(arrOfInput[i]);
+     // console.log(arrOfInput[i+1]);
+     // console.log(arrOfInput[i]);
     // var condition =  arrOfInput[i+1] - arrOfInput[i]
-     if((arrOfInput[i+1] - arrOfInput[i]) > 1 ){
+     if((arrOfInput[i+1] - arrOfInput[i]) > 1 ){ //if the diff btwen entires is more than 1 ie. they are not consecutive
      console.log(arrOfInput[i] + 1);
+     str = String.fromCharCode((arrOfInput[i] + 1)); //get the char that's missing
+     console.log(str);
+     return str
      }
-       }
-   
-  
-  
-  //return str;
+     else {
+         str = undefined;
+         //console.log(str);
+     }
+     }
+        return str;
 
 }
 
 
 
 
-fearNotLetter('abce');// 'd'
-/*fearNotLetter('bcd');// undefined
+//fearNotLetter('abce');// 'd'
+//fearNotLetter('bcd');// undefined
 fearNotLetter('abcdefghjklmno');// 'i'
-fearNotLetter('yz');// undefined*/
+//fearNotLetter('yz');// undefined*/
