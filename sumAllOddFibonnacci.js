25c55bde642daf570ca05dcd19169a47d0168d21
Dee Bryant <tolkadot@gmail.com>
2015-08-08 17:18:05 +1000

    Update sumAllOddFibonnacci.js

--- sumAllOddFibonnacci.js
+++ sumAllOddFibonnacci.js
@@ -5,23 +5,36 @@
 function sumFibs(num) {
     
     var fibArr = [1, 1];
+    var fibArrOdd =[];
+    var total = 0;
     
-    for (i=0; i<num; i ++){
-        console.log((fibArr[i] + fibArr[i+1])%2);
-        //if(((fibArr[i] + fibArr[i+1])%2) !== 0){
-        fibArr.push(fibArr[i] + fibArr[i+1]);
-        //}
-      
+    for (i=0; i<num; i ++){ //generate fib series
+     fibArr.push(fibArr[i] + fibArr[i+1]);
       }
     console.log(fibArr);
+    
+    for (i=0; i<num; i++) { //fib series  of odd nums up to num
+        if((fibArr[i]%2 ===1) && (fibArr[i] <= num) )
+        fibArrOdd.push(fibArr[i]);
+        }
+    
+    for(i=0; i<=fibArrOdd.length; i++) //sum them up 
+    {
+         total = total + fibArrOdd[i];
+    }
+    
+    //console.log(fibArrOdd);
+    console.log(total);
+    
+    
     
   //return num;
 }
 
-sumFibs(10);
-//sumFibs(1);
-//sumFibs(1000); //1785
-//sumFibs(4000000);// 4613732
-//sumFibs(4);// 5
-//sumFibs(75024);// 60696
-//sumFibs(75025);// 135721
+sumFibs(1000);
+sumFibs(1);
+sumFibs(1000); //1785
+sumFibs(4000000);// 4613732
+sumFibs(4);// 5
+sumFibs(75024);// 60696
+sumFibs(75025);// 135721
