e26f5a4302f79f60dfda1268d62c878fde03c650
Dee Bryant <tolkadot@gmail.com>
2015-08-09 11:34:25 +1000

    Update sumOfAllPrimes.js

--- sumOfAllPrimes.js
+++ sumOfAllPrimes.js
@@ -1,10 +1,10 @@
-//Sum all the prime numbers up to and including the provided number.
+/Sum all the prime numbers up to and including the provided number.
 //dbryant 8aug15
 
 function sumPrimes(num) {
  var arr = [];
- var newarr = [1];
- for(i=1; i<=num; i++) //arr of all the numbers 1 to num
+ var newarr = [];
+ for(i=1; i<=num; i++) //arr of all the odd numbers 1 to num & 2
  {
      if(i%2 !== 0 || i ===2 ){
      arr.push(i);
@@ -13,12 +13,14 @@ function sumPrimes(num) {
   console.log(arr);
   
   
- for(i=0; i<arr.length; i++){  //for every number in the array
+ for(i=0; i<arr.length; i++){  //for every number in the odd numbers array
+ console.log(arr.indexOf(arr[i]));
  
-  for(j=0; j<arr.length; j++) {
+ 
+  for(j=0; j<arr.indexOf(arr[i]); j++) {
       if(arr[i]%arr[j] >1 && i!=j){ //if the number is prime
           newarr.push(arr[i]); //push it to a new array
-          //console.log(newarr);
+          console.log(newarr);
       }
   }
  console.log(newarr);
