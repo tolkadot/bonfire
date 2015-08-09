df8496ba9d2dd68286eab54de004ca16694c77a8
Dee Bryant <tolkadot@gmail.com>
2015-08-09 14:38:52 +1000

    Update sumOfAllPrimes.js

--- sumOfAllPrimes.js
+++ sumOfAllPrimes.js
@@ -1,11 +1,12 @@
 //Sum all the prime numbers up to and including the provided number.
-//dbryant 8aug15
+//dbryant 9aug15 @tolkadot
 
 function sumPrimes(num) {
-    var sum = 0;
+ var sum = 0;
  var arrOdds = [];
  var primeArr = [1, 2];
  var modulusArr = []; //arr for holding the outcome of modulus operation.
+ 
  for(i=1; i<=num; i++) //arr of all the odd numbers 1 to num & 2
  {
      if(i%2 !== 0 || i ===2 ){
@@ -13,33 +14,27 @@ function sumPrimes(num) {
      }
  }
 
-console.log(arrOdds);
 for (i=2; i<arrOdds.length; i++) {
-var tempNum = arrOdds[i]; //tempNum will be the next number in arrOdds to check
-  for(var n=2; n<tempNum; n++) { 
-      modulusArr.push(tempNum%n); //push the outcome of mod operator to array.
+  for(var n=2; n<arrOdds[i]; n++) { 
+      modulusArr.push(arrOdds[i]%n); //push the outcome of mod operator to array.
       }
       if(modulusArr.indexOf(0) == -1){ //if the modulus of the calc is never 0
-       primeArr.push(tempNum); //the number is prime - push it to ne
+       primeArr.push(arrOdds[i]); //the number is prime - push it to primeArr
       }
        modulusArr =[]; //reset the modulus array
-      
 }
-console.log(primeArr);
 
-for(i=0; i<primeArr.length; i++)
+
+for(i=1; i<primeArr.length; i++)
 {
-     sum = sum+primeArr[i]
+     sum = sum+primeArr[i]; //add all the primes together.
+     num =sum;
     
 }
+return num;
 
 }
  
-//console.log(arrOdds);
- 
- // return num;
-
-
-sumPrimes(13);
-//sumPrimes(10);// 17
-//sumPrimes(977);// 73156
+sumPrimes(10);
+sumPrimes(10);// 17
+sumPrimes(977);// 73156
