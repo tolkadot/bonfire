738157c3c92f060d55934cc0a93fa5aa15ae8a22
Dee Bryant <tolkadot@gmail.com>
2015-08-09 15:27:04 +1000

    Update smallestCommon.js

--- smallestCommon.js
+++ smallestCommon.js
@@ -4,27 +4,32 @@
 function smallestCommons(arr) {
     
     var rangeArr=[]; //array of the full range of numbers arr[0] to arr[1]
-    var modulusArr=[];
+    var modulusArr=[]; //array of the lowest denominators to be removed from rangeArr
     arr = arr.sort(function(a, b){ //sort the array lowest to highest
         return a-b;
     });
-    console.log(arr);
+    //console.log(arr);
     
     for(i=0; i<arr[1]; i++){ 
         rangeArr.push(i+1); 
     }
     console.log(rangeArr);
-    
-    for(i=rangeArr.length -1; i===1; i--)
-    { console.log(i)}
-    
-    
-    
-    
+  
+  var tempNum = 15; //where tempNum = next entry in rangeArr
+  
+  for(i=2; i<tempNum; i++){
+        //modulusArr.push(tempNum%i);
+        if(tempNum%i ===0)
+        { 
+         modulusArr.push(i); //
+          //console.log(i);
+        }
+        console.log(modulusArr);
+  }
   //return arr;
 }
 
-smallestCommons([5,1]);// 60
-smallestCommons([1,5]);
+smallestCommons([7,1]);// 60
+//smallestCommons([1,5]);
 //smallestCommons([5,1]);// 60
 //smallestCommons([1,13]);// 360360
