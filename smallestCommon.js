a75186ccbb4cd35ea51dc35ca8ac9f726f7313e7
Dee Bryant <tolkadot@gmail.com>
2015-08-09 14:40:59 +1000

    Create smallestCommon.js

---
+++ smallestCommon.js
@@ -0,0 +1,11 @@
+//Find the smallest number that is evenly divisible by all numbers in the provided range.
+//dbryant 9aug15 @tolkadot
+
+function smallestCommons(arr) {
+  return arr;
+}
+
+smallestCommons([5,1]);// 60
+//smallestCommons([1,5]);
+//smallestCommons([5,1]);// 60
+//smallestCommons([1,13]);// 360360
