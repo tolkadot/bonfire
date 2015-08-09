d32a3e15cc2413b75f4b0cd48739fa2c1deba6f4
Dee Bryant <tolkadot@gmail.com>
2015-08-09 13:35:52 +1000

    Update sumOfAllPrimes.js

--- sumOfAllPrimes.js
+++ sumOfAllPrimes.js
@@ -1,37 +1,39 @@
-/Sum all the prime numbers up to and including the provided number.
+//Sum all the prime numbers up to and including the provided number.
 //dbryant 8aug15
 
 function sumPrimes(num) {
- var arr = [];
+ var arrOdds = [];
  var newarr = [];
  for(i=1; i<=num; i++) //arr of all the odd numbers 1 to num & 2
  {
      if(i%2 !== 0 || i ===2 ){
-     arr.push(i);
+     arrOdds.push(i);
      }
  }
-  console.log(arr);
+  console.log(arrOdds);
   
   
- for(i=0; i<arr.length; i++){  //for every number in the odd numbers array
- console.log(arr.indexOf(arr[i]));
+ for(i=0; i<arrOdds.length; i++){  //for every number in the odd numbers array
+
+//console.log(i);
+ //console.log(arrOdds.indexOf(arr[i]));
  
  
-  for(j=0; j<arr.indexOf(arr[i]); j++) {
-      if(arr[i]%arr[j] >1 && i!=j){ //if the number is prime
-          newarr.push(arr[i]); //push it to a new array
-          console.log(newarr);
+  for(j=2; j<arrOdds.indexOf(arrOdds[i]); j++) {
+      //if(arrOdds[i]%arrOdds[j] >1 && i!=j){ //if the number is prime
+        //  newarr.push(arrOdds[i]); //push it to a new array
+         //console.log(i , j, arrOdds[i] , arrOdds[j], arrOdds.indexOf(arrOdds[i]), " -----");
       }
   }
- console.log(newarr);
+// console.log(newarr);
  }
      
  
  
- //console.log(arr);
+ //console.log(arrOdds);
  
  // return num;
-}
+
 
 sumPrimes(10);
 //sumPrimes(10);// 17
