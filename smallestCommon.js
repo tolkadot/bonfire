feeb0b0ebbd3ec055cdf1c298169250cc5a41702
Dee Bryant <tolkadot@gmail.com>
2015-08-10 20:00:37 +1000

    Update smallestCommon.js

--- smallestCommon.js
+++ smallestCommon.js
@@ -1,51 +1,47 @@
 //Find the smallest number that is evenly divisible by all numbers in the provided range.
-//dbryant 9aug15 @tolkadot
+//This is probably not the best mathematical approach to this problem, but it is effective
+//none the less.
+
+//dbryant 10aug15 @tolkadot
 
 function smallestCommons(arr) {
 
     var rangeArr = []; //array of the full range of numbers arr[0] to arr[1]
-    var modulusArr = []; //array of the lowest denominators to be removed from rangeArr
     arr = arr.sort(function (a, b) { //sort the array lowest to highest
         return a - b;
     });
-    //console.log(arr);
+    
 
     for (i = 0; i < arr[1]; i++) {
-        rangeArr.push(i + 1);
+        rangeArr.push(i + 1); //array of the full range of numbers arr[0] to arr[1]
     }
-    console.log(rangeArr);
-    console.log(rangeArr[4]);
 
-
+// function to retun the modulus of two numbers
     function checkNext(number1nextnum, number2rangenum) {
         return number1nextnum % number2rangenum;
 
     }
 
-    var nextNum = 6;
-   // console.log(nextNum);
+    var nextNum = arr[1] + 1; //start trial and error testing at the next number
 
     for (i = 1; i <= rangeArr.length; i++) {
-        if (checkNext(nextNum, rangeArr[i]) >= 1)
-
+        //if there is a modulus check the next number & reset the divisor 
+        if (checkNext(nextNum, rangeArr[i]) >= 1) 
         {
-            console.log("checkd this time", nextNum, rangeArr[i], 'if');
             nextNum++;
             i = 1;
-            console.log("gonna check next", nextNum, rangeArr[i]);
-        } else {
+        } 
+        //otherwise try the next divisor
+        else { 
             checkNext(nextNum, rangeArr[i]);
-            console.log(nextNum, rangeArr[i],'else' ,i);
         }
 
     }
- //console.log(rangeArr[4]);
-    //console.log(nextNum);
-
-    //console.log(checkNext(8, 2));
+ 
+    console.log(nextNum);
 
 }
-smallestCommons([1,13]); // 60
+smallestCommons([1,5]); // 60
 //smallestCommons([1,5]);
 //smallestCommons([5,1]);// 60
 //smallestCommons([1,13]);// 360360
