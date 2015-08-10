27908fedbbfa1b414a72bb0c01646c8f79ac4cee
Dee Bryant <tolkadot@gmail.com>
2015-08-10 20:34:28 +1000

    Update findersKeepers.js

--- findersKeepers.js
+++ findersKeepers.js
@@ -1,11 +1,23 @@
 //Create a function that looks through an array (first argument) and returns 
 //the first element in the array that passes a truth test (second argument).
-
-
 //dbryant 10aug15 @tolkadot
 function find(arr, func) {
+    
+    //arr = [1, 2, 3, 4]
+    // func = function(num){ return num % 2 === 0; }
+    
   var num = 0;
-  return num;
+    for(i=0; i<arr.length; i++) {
+        num = arr[i];
+        if(func(num) === true){
+            console.log(num);
+        return num;
+        }
+  
+    }
+    
+    
+//    return num;   
 }
 
 find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
