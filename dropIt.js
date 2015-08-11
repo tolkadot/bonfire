133853cfdc43c657ac3a97a457fd94d92968b353
Dee Bryant <tolkadot@gmail.com>
2015-08-11 14:10:57 +1000

    Update dropIt.js

--- dropIt.js
+++ dropIt.js
@@ -3,13 +3,26 @@
 //dbryant 10aug15 @tolkadot
 
 function drop(arr, func) {
-  // Drop them elements.
+ //arr = [1, 2, 3, 4]
+ // function(n) {return n >= 3; }
+    
+    for(i=0; i<=arr.length; i++)
+    {   
+    
+    var outcome = func(arr[i]);
+        if(outcome === false)       {
+        arr.shift();
+            console.log(arr);
+        }
+        }
+    
+    
   return arr;
 }
 
-drop([1, 2, 3], function(n) {return n < 3; });
 
 
 drop([1, 2, 3, 4], function(n) {return n >= 3; });//[ 3, 4 ]
-drop([1, 2, 3], function(n) {return n > 0; });//[1, 2, 3];
-drop([1, 2, 3, 4], function(n) {return n > 5; });//[]
+//drop([1, 2, 3], function(n) {return n > 0; });//[1, 2, 3];
+//drop([1, 2, 3, 4], function(n) {return n > 5; });//[]
+//drop([1, 2, 3], function(n) {return n < 3; });
