127e59c692f9cf366b7d49a3a6d98b018681dc35
Dee Bryant <tolkadot@gmail.com>
2015-08-11 17:51:39 +1000

    Update dropIt.js

--- dropIt.js
+++ dropIt.js
@@ -1,32 +1,34 @@
 //Drop the elements of an array (first argument), starting from the front, 
 //until the predicate (second argument) returns true.
-//dbryant 10aug15 @tolkadot
+//dbryant 11aug15 @tolkadot
 
 function drop(arr, func) {
- //arr = [1, 2, 3, 4]
- // function(n) {return n >= 3; }
-    
-    
-    
-    for(var i = 0; i<=arr.length; i++)
-    {   
-        console.log(i);
-    switch (func(arr[i])) {
-  case false:
-    arr.shift();
-    i = 0;
-    console.log(i);
-    break;
-  case true:
-    return arr;
+
+    for(i = 0; i<arr.length;){ //for the first element in the array
   
+  
+  if (arr === []) // if the array is empty return it
+  {
+      return arr;
+  }
+   else if(func(arr[i]) === false) // if the function elavs to false shift the array
+    {
+        console.log('false');
+        arr.shift();
+        console.log(arr);
+    }
+    
+    else {
+   console.log('true');// if the function elavs to true return the array
+   return arr;
     }
     }
-     
-  console.log( arr);
+   
+  return arr;
+
 }
 
-drop([1, 2, 3, 4], function(n) {return n >= 3; });//[ 3, 4 ]
+//drop([1, 2, 3, 4], function(n) {return n >= 3; });//[ 3, 4 ]
 //drop([1, 2, 3], function(n) {return n > 0; });//[1, 2, 3];
-//drop([1, 2, 3, 4], function(n) {return n > 5; });//[]
+drop([1, 2, 3, 4], function(n) {return n > 5; });//[]
 //drop([1, 2, 3], function(n) {return n < 3; });
