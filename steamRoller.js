6e4d85b2ad090adb2f9c51ed589aa3d036403535
Dee Bryant <tolkadot@gmail.com>
2015-08-12 19:55:36 +1000

    Update steamRoller.js

--- steamRoller.js
+++ steamRoller.js
@@ -2,25 +2,27 @@
 //dbryant 11aug15 @tolkadot
 
 function steamroller(arr) {
-  var flatArr = [];
-  var shifted;
-       
- if(Array.isArray(arr)){
-    shifted = arr.shift();
-     console.log(arr, shifted);
- }
-     else {
-     flatArr.push(shifted);
-     }
+    console.log('hello');
+    var flatArr = [];
+    var shifted;
+    var contents;
 
+    
+ 		Array.isArray(arr); 
+        shifted = arr.shift();
+            while(shifted !== [])
+            {
+              contents = shifted.shift();
+              console.log(contents);
+            }
+    
+    
 
-     
-  
-  console.log(arr, shifted, flatArr);
+    console.log(arr, shifted, flatArr);
 
-
-steamroller([[['a']], [['b']]]);
+    
 }
+steamroller([[['a']], [['b']]]);
 //steamroller([[['a']], [['b']]]); //, ['a', 'b'], 'should flatten nested arrays');should flatten nested arrays: expected [ [ [ 'a' ] ], [ [ 'b' ] ] ] to deeply equal [ 'a', 'b' ]
 //steamroller([1, [2], [3, [[4]]]]); //, [1, 2, 3, 4], 'should flatten nested arrays');should flatten nested arrays: expected [ 1, [ 2 ], [ 3, [ [Object] ] ] ] to deeply equal [ 1, 2, 3, 4 ]
 //steamroller([1, [], [3, [[4]]]]); //, [1, 3, 4], 'should work with empty array
