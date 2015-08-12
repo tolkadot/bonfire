7f65be909fa1b29246626ed819a3abd52233e9c6
Dee Bryant <tolkadot@gmail.com>
2015-08-12 18:40:01 +1000

    Update steamRoller.js

--- steamRoller.js
+++ steamRoller.js
@@ -2,13 +2,24 @@
 //dbryant 11aug15 @tolkadot
 
 function steamroller(arr) {
-  // I'm a steamroller, baby
-  return arr;
+ 
+  var shifted;
+    var shifted2;
+ if(Array.isArray(arr)){
+    shifted = arr.shift();
+     if(Array.isArray(shifted))
+     {
+     shifted2 = shifted.shift();
+     }
+ 
+  console.log(arr, shifted);
+  console.log(arr, shifted2);
+ }
 }
 
-steamroller([1, [2], [3, [[4]]]]);
+steamroller([['a']]);
 
 
-steamroller([[['a']], [['b']]]); //, ['a', 'b'], 'should flatten nested arrays');should flatten nested arrays: expected [ [ [ 'a' ] ], [ [ 'b' ] ] ] to deeply equal [ 'a', 'b' ]
-steamroller([1, [2], [3, [[4]]]]); //, [1, 2, 3, 4], 'should flatten nested arrays');should flatten nested arrays: expected [ 1, [ 2 ], [ 3, [ [Object] ] ] ] to deeply equal [ 1, 2, 3, 4 ]
-steamroller([1, [], [3, [[4]]]]); //, [1, 3, 4], 'should work with empty arrays');should work with empty arrays: expected [ 1, [], [ 3, [ [Object] ] ] ] to deeply equal [ 1, 3, 4 ]
+//steamroller([[['a']], [['b']]]); //, ['a', 'b'], 'should flatten nested arrays');should flatten nested arrays: expected [ [ [ 'a' ] ], [ [ 'b' ] ] ] to deeply equal [ 'a', 'b' ]
+//steamroller([1, [2], [3, [[4]]]]); //, [1, 2, 3, 4], 'should flatten nested arrays');should flatten nested arrays: expected [ 1, [ 2 ], [ 3, [ [Object] ] ] ] to deeply equal [ 1, 2, 3, 4 ]
+//steamroller([1, [], [3, [[4]]]]); //, [1, 3, 4], 'should work with empty array
