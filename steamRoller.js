6621e6ab7b2662d516b36a282d1796571a8bae61
Dee Bryant <tolkadot@gmail.com>
2015-08-13 18:14:42 +1000

    Update steamRoller.js

--- steamRoller.js
+++ steamRoller.js
@@ -1,44 +1,28 @@
-//Flatten a nested array. You must account for varying levels of nesting.
-//dbryant 11aug15 @tolkadot
+function steamroller(arr){
+    
+    var flatArr = [];
+    var isArr = false;
+    
+    for(i=0; i<arr.length; i++)
+    {
+        if(Array.isArray(arr)){
+            isArr = true;
+        }
+    }
+    
+    if(isArr) {
+        
+        for(var j =0; j<arr.length; j++){
+            newArr = newArr.concat.arr[j];
+        }
+        steamroller(newArr);
+    }
+    else {
+        return(newArr);
+    }
+        
+        
+    }
+    
 
-/*function steamroller(arr) {
-  var flatArr = [];
-  var shifted;
-  
-  
-      
-  /*    console.log(a, b);
-  return a.concat(b); }
-  
-
-for(i=0; i<=arr.length; i++){
-    console.log(i);
-  shifted = arr.reduce(shrink);
-  console.log(shifted);
-}
-
-
-
-console.log(shifted);
-
-}*/
-
-function shrink(a, b){
-      if(isArray.Array(a)) {
-          arr.reduce();
-          console.log(a, b, arr)
-           console.log(a, b);
-  return a.concat(b);
-      }
-      else { console.log('not array')}
-  }
-
-shrink([1, [2]]);  
-
-
-
-
-
-//steamroller([[['a']], [['b']]]); //, ['a', 'b'], 'should flatten nested arrays');should flatten nested arrays: expected [ [ [ 'a' ] ], [ [ 'b' ] ] ] to deeply equal [ 'a', 'b' ]
-//steamroller([1, [2], [3, [[4]]]]); //, [1, 2, 3, 4], 'should flatten nested arrays');should flatten nested arrays: expected [ 1, [ 2 ], [ 3, [ [Object] ] ] ] to deeply equal [ 1, 2, 3, 4 ]
-//steamroller([1, [], [3, [[4]]]]); //, [1, 3, 4], 'should work with empty array
+steamroller([1, [2], [3, [[4]]]]);
