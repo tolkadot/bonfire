22007835a8b8d8a6cfb402ef70a2b8280b03d516
Dee Bryant <tolkadot@gmail.com>
2015-08-13 20:10:23 +1000

    Update binaryAgent.js

--- binaryAgent.js
+++ binaryAgent.js
@@ -1,13 +1,12 @@
 //Return an English translated sentence of the passed binary string.
 
-function binaryAgent(str) {
-    var counter = 0;
-    var binArr = [];
-    var binNum = [];
-    var decArr = [];
-    var strArr = [];
-    var tempString;
-    binArr = str.split(' ');
+//binary to decimal converter
+function binaryToDecimal(str) {
+var binArr = [];
+var binNum = [];
+var counter = 0;
+var decArr = [];
+binArr = str.split(' ');
   
   for(i=0; i<binArr.length; i++) {
         counter = 0;
@@ -29,24 +28,29 @@ function binaryAgent(str) {
         if(binNum[7] === '1'){
         counter += 1;}
         decArr.push(counter);
-  }
+  } 
+  return decArr;
+ }
+
+
+function binaryAgent(str) {
+    
+    decArr = binaryToDecimal(str); //convert binary to decimal
+    var strArr = [];
+    var tempString; 
  
-  console.log(decArr);
-  
  for(j=0; j<decArr.length; j++)
   {
       tempString = String.fromCharCode(decArr[j]);
-      //console.log(tempString);
       strArr.push(tempString);
-      
   }
-  console.log(strArr);
-  str = strArr.join(',');
-  console.log(str);
-  str.replace(/,/ , '');
- console.log(str);
+  
+  str = strArr.join(',').replace(/,/g , '');
+ return str;
+ 
 }
 
+binaryToDecimal('01000001 01110010');
 
 binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
 
