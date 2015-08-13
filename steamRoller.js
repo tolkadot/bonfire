6fcb27c994a7760478ded98bbdc9be037ede9641
Dee Bryant <tolkadot@gmail.com>
2015-08-13 19:06:03 +1000

    Update steamRoller.js

--- steamRoller.js
+++ steamRoller.js
@@ -2,29 +2,29 @@ function steamroller(arr){
     
     var flatArr = [];
     var isArr = false;
-    
+  //check if its an array or not.  
     for(i=0; i<arr.length; i++)
     {
-        //console.log(arr[i]);
         if(Array.isArray(arr[i])){
-          isArr = true;
-        //console.log(isArr);
+          isArr = true; 
+          console.log(isArr, i );
         }
-        
-    }
-    
+    }    
+  
     if(isArr) {
         
         for(var j =0; j<arr.length; j++){
             flatArr = flatArr.concat(arr[j]);
             console.log(flatArr);
+            
         }
         steamroller(flatArr);
     }
     else {
+        console.log(flatArr);
         return(flatArr);
     }
-   
+    
     }
     
 
