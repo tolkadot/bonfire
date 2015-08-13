323207549afcdb1642d448ef90fb35631d593bf2
Dee Bryant <tolkadot@gmail.com>
2015-08-13 19:18:10 +1000

    Create binaryAgent.js

---
+++ binaryAgent.js
@@ -0,0 +1,30 @@
+//Return an English translated sentence of the passed binary string.
+
+function binaryAgent(str) {
+    
+    var binArr = [];
+    var binNum = [];
+  binArr = str.split(' ')
+  
+  for(i=0; i<binArr.length; i++) {
+       binNum = binArr[i];
+        for (j=0; j<binNum.length j++)
+          
+      
+      console.log(binNum[0]);
+  }
+    
+  //console.log(binArr[0]);
+  
+//  return str;
+}
+
+
+
+
+binaryAgent('01000001 01110010')
+//binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
+
+
+
+//dbryant 13aug15 @tolkadot
