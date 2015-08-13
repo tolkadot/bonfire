3c87e1eda5914e0bc4d7a4f752f9a82016e04718
Dee Bryant <tolkadot@gmail.com>
2015-08-13 19:34:39 +1000

    Update binaryAgent.js

--- binaryAgent.js
+++ binaryAgent.js
@@ -1,29 +1,45 @@
 //Return an English translated sentence of the passed binary string.
 
 function binaryAgent(str) {
-    
+    var counter = 0;
     var binArr = [];
     var binNum = [];
-  binArr = str.split(' ')
+    var decArr = [];
+  binArr = str.split(' ');
   
   for(i=0; i<binArr.length; i++) {
+      counter = 0;
        binNum = binArr[i];
-        for (j=0; j<binNum.length j++)
-          
+       console.log(binNum[0], binNum[1], binNum[2], binNum[3]);
+        if (binNum[0] === '1'){
+            console.log("hello");
+        counter = counter + 128;}
+        if( binNum[1] === '1'){
+        counter += 64;}
+        if(binNum[2] === '1')
+        counter += 32;
+        if(binNum[3] === '1')
+        counter += 16;
+        if(binNum[4] === '1')
+        counter += 8;
+        if(binNum[5] === '1')
+        counter += 4;
+        if(binNum[6] === '1')
+        counter += 2;
+        if(binNum[7] === '1')
+        counter += 1;
+     // console.log(binNum[0], counter);
+      decArr.push(counter);
+      console.log(decArr);
       
-      console.log(binNum[0]);
   }
-    
-  //console.log(binArr[0]);
+  for(j=0; j<decArr.length(); j++)
   
-//  return str;
+  String.fromCharCode(decArr[0]);
 }
 
 
-
-
-binaryAgent('01000001 01110010')
-//binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
+binaryAgent('01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111');
 
 
 
