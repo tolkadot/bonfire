9c9d76df23dc60b8bf42ab2ab08c91872b4d8674
Dee Bryant <tolkadot@gmail.com>
2015-08-16 19:46:40 +1000

    Update argumentsOptional.js

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -1,10 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////
+//Create a function that sums two arguments together. If only one argument is provided, 
+//return a function that expects one additional argument and will return the sum
+//dbryant 14aug15 @tolkadot
+////////////////////////////////////////////////////////////////////////////
+
+
  
 function add() {
     console.log(arguments.length);
-    
+  
+  
 if (arguments.length === 1) {
  
-  function(x) { return x + arguments[0] }
+  return function(x){ return (x + arguments[0]); };
 }
    
 
