22610f7f4f1be67545954e136ba3530cfaf2817d
Dee Bryant <tolkadot@gmail.com>
2015-08-17 19:41:20 +1000

    Update argumentsOptional.js

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -8,23 +8,27 @@
  
 function add() {
   
- var z = arguments[0];
- //console.log(isNaN(arguments[1]) );
- console.log(typeof(arguments[0]),typeof(arguments[1]) );
- 
- if (typeof(arguments[0]) != 'number') {
+ var z = arguments[0];  // rename the first argument so that we can use it in our new function
+
+//if we have one input and its a number return a function that uses a second number.
+//make sure that the new function had a valid imput.
+  if(arguments.length === 1 && typeof(arguments[0]) === 'number') {
+       return function(x){ 
+     console.log(typeof(x));
+     if(typeof(x) === 'number'){
+     return (x + z); }};} //return the sum of the two inputs
+    
+     // check if either of the two arguments are not numbers - return undefined if they are
+if ((typeof(arguments[0]) != 'number') || (typeof(arguments[1]) != 'number')) {
   return;
  }
-    
- if(arguments.length === 1) { return function(x){ 
-     console.log(x+z)
-     return (x + z); };}
-   
-   else {console.log(arguments[0]+arguments[1])
+//otherwise return the sum of both.
+   else {console.log(arguments[0]+arguments[1]);
        return (arguments[0]+arguments[1]);}
 
 }
-add(2, 3);
-add(2)(3);
-add(2, '3');
+//add(2, 3);
+//add(2)(3);
+//add(2, '3');
 add(2)([3]);
+//add('http://bit.ly/IqT6zt');
